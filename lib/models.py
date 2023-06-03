c30679c457a46354fbe8afd0e07352306d61226d
+ <+>
2023-06-03 05:14:46 +0300

    Done with Assignment

--- lib/models.py
+++ lib/models.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import (Column, String, Integer)
+from sqlalchemy import (PrimaryKeyConstraint, Column, String, Integer)
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class Dog(Base):
     __tablename__ = 'dogs'
+    __table_args__ = (PrimaryKeyConstraint('dog_id'),)
 
-    id = Column(Integer(), primary_key=True)
-    name = Column(String())
-    breed = Column(String())
+    dog_id = Column(Integer())
+    dog_name = Column(String())
+    dog_breed = Column(String())
+
+    
